a1dcbde0d45dc534959fbfafce74d193a61ca264
PabitraBansal <bansalpabitra@hotmail.com>
2019-05-24 10:56:49 +0530

    Initail Commit

---
+++ app/src/main/java/com/example/kawangigame/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kawangigame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText simpleEditText = (EditText) findViewById(R.id.editText);
+        String editTextValue = simpleEditText.getText().toString();
+        int colourNum = Integer.parseInt(editTextValue);
+        if (colourNum >= 8 && colourNum <= 12)
+            Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+}
